b7f9b6a34e165f48672bb745b56d3ef6b7ba1c27
Andrew <ward.spacecase@gmail.com>
2022-12-15 17:59:35 -0700

    updated for top 3 elves

--- day-1/main.py
+++ day-1/main.py
@@ -3,22 +3,40 @@ elfTotal = []
 
 currentTotal = 0
 
+# open text file input.txt
 with open('input.txt', 'r') as f:
+
+	# loop over everyline
 	for line in f:
-		print(currentTotal)
+		# check for new line - indicates new elf
 		if line == "\n":
+			# make sure not adding empty zeros
 			if currentTotal != 0:
+				# add total to array
 				elfTotal.append(currentTotal)
+			# reset total
 			currentTotal = 0
+		# check for empty/end of page
 		elif line == '':
 			print("hello")
+		# add to total
 		else:
 			currentTotal = currentTotal + int(line)
 
 	else:
-		# No more lines to be read from file
-		maxIndex = max(elfTotal)
-		print(elfTotal)
-		print(maxIndex)
+		# sort largest to smallest
+		elfTotal.sort(reverse=True)
+
+		# print 3 largest (index 0-2)
+		print(elfTotal[0])
+		print(elfTotal[1])
+		print(elfTotal[2])
+
+		# total top 3
+		totalTopThree = elfTotal[0] + elfTotal[1] + elfTotal[2]
+
+		# print top 3
+		print("\nSum: " + str(totalTopThree))
+
 		f.close()
 
